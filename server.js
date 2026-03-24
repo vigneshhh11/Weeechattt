a1275542cbcc4705c4991a7672551be38ce9145b
vigneshhh11 <sreevigneswar67@gmail.com>
2026-03-24 10:10:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,36 @@
 const express = require("express");
 const http = require("http");
-const path = require("path");
 const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server, {
-  cors: { origin: "*" }
+    cors: {
+        origin: "*"
+    }
 });
 
-// Serve your HTML
-app.use(express.static(__dirname));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// Temporary message storage
+// store messages (temporary)
 let messages = [];
 
 io.on("connection", (socket) => {
-  console.log("User connected");
+    console.log("User connected");
 
-  socket.emit("load_messages", messages);
+    // send old messages
+    socket.emit("load_messages", messages);
 
-  socket.on("send_message", (data) => {
-    messages.push(data);
-    io.emit("receive_message", data);
-  });
+    socket.on("send_message", (data) => {
+        messages.push(data);
+
+        // send to ALL users
+        io.emit("receive_message", data);
+    });
 });
 
-const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
 
-server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
