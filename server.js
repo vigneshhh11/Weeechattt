0c0d38d0cfcad9eda400736ff25f37bb3596f580
vigneshhh11 <sreevigneswar67@gmail.com>
2026-03-24 10:32:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,22 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
+
 const io = new Server(server, {
     cors: {
         origin: "*"
     }
 });
 
-// store messages (temporary)
 let messages = [];
 
 io.on("connection", (socket) => {
-    console.log("User connected");
+    console.log("User connected:", socket.id);
 
-    // send old messages
     socket.emit("load_messages", messages);
 
     socket.on("send_message", (data) => {
         messages.push(data);
-
-        // send to ALL users
         io.emit("receive_message", data);
     });
 });
@@ -31,6 +28,8 @@ app.get("/", (req, res) => {
     res.send("Server is running");
 });
 
-server.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log("Server running on port", PORT);
 });
